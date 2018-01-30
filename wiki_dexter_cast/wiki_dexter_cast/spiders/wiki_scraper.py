ce4454bc4a05edb9dc7000ecf743c4bb43fb65e2
Shenghao1993 <wangshenghao1993@gmail.com>
2018-01-30 18:34:18 +0800

    completetd wiki scraper

--- wiki_dexter_cast/wiki_dexter_cast/spiders/wiki_scraper.py
+++ wiki_dexter_cast/wiki_dexter_cast/spiders/wiki_scraper.py
@@ -2,27 +2,53 @@ import scrapy
 
 class WikiDexterCast(scrapy.Spider):
 	name = "dexter"
-	start_urls = ["https://en.wikipedia.org/wiki/Dexter_(season_1)"]
+	start_urls = ['https://en.wikipedia.org/wiki/Dexter_(season_1)']
 
 	def parse(self, response):
 		for url in self.start_urls:
 			yield scrapy.Request(url, callback=self.parse_cast)
 
-	# def parse_list(self, response):
-	# 	list_wrapper = 
-
 	def parse_cast(self, response):
-		cast_lists = []
-		parent_dir = response.xpath("//div[@class='mw-parser-output']//ul")
+		mainCasts = response.xpath("//div[@class='mw-parser-output']/ul[1]//li")
+		count = 1
 
-		for cast_list in cast_lists:
+		for cast in mainCasts:
+			sel = "//div[@class='mw-parser-output']/ul[1]//li[%d]" % count
+			count += 1
+			actorName = cast.xpath(sel + "//a[1]/text()").extract()
+			actorLink = "https://en.wikipedia.org" + cast.xpath(sel + "//a[1]/@href").extract()[0]
 			yield {
-				'Name':
-				'Type':
-				'Link':
+				'Name': actorName,
+				'Type': "actor",
+				'Link':	actorLink
 			}
 
-		# Getting Actor/Character Name
-		
+			characterName = cast.xpath(sel + "//a[2]/text()").extract()
+			characterLink = "https://en.wikipedia.org" + cast.xpath(sel + "//a[2]/@href").extract()[0]
+			yield {
+				'Name': characterName,
+				'Type': "character",
+				'Link': characterLink
+			}
 
-		yield item
+		casts = response.xpath("//table[@class='multicol']//ul//li")
+		count = 1
+
+		for cast in casts:
+			sel = "//table[@class='multicol']//ul//li[%d]" % count
+			count += 1
+			actorName = cast.xpath(sel + "//a[1]/text()").extract()
+			actorLink = "https://en.wikipedia.org" + cast.xpath(sel + "//a[1]/@href").extract()[0]
+			yield {
+				'Name': actorName,
+				'Type': "actor",
+				'Link':	actorLink
+			}
+
+			characterName = cast.xpath(sel + "//a[2]/text()").extract()
+			characterLink = "https://en.wikipedia.org" + cast.xpath(sel + "//a[2]/@href").extract()[0]
+			yield {
+				'Name': characterName,
+				'Type': "character",
+				'Link': characterLink
+			}
